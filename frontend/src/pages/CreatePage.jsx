14b46b6593bb607c42a27a9b0807046dcff73fdd
Stefan Schroeder <schroeder.stefan10@googlemail.com>
2025-07-24 15:17:16 +0200

    implement HomePage.jsx

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -4,7 +4,6 @@ import {useState} from "react"
 import api from "../lib/axios";
 
 const CreatePage = () => {
-  console.log('MyComponent rendered at:', new Date().toISOString());
   const [formData, setFormData] = useState({
     name: '',
     stueckzahl: '',
