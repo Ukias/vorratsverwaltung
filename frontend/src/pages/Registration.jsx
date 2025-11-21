26c7a0098054574ac51e04aa8d2197d95f89f292
Stefan Schroeder <schroeder.stefan10@googlemail.com>
2025-11-21 11:10:07 +0100

    add tailwind to Registration.jsx

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -30,33 +30,39 @@ const Registration = () => {
     }
 
   return (
-    <div className="registration-container">
-        <h1>Registration</h1>
-        <form className="registration-form" onSubmit={handleSubmit}>
-            <div className="form-group">
-                <label htmlFor="email">Email</label>
-                <input 
-                    type="text"
-                    id="email"
-                    name="email"
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="Enter Email"
-                    required />
-            </div>
-            <div className="form-group">
-                <label htmlFor="password">Password</label>
-                <input 
-                    type="password"
-                    id="password"
-                    name="password"
-                    onChange={(e) => setPassword(e.target.value)}
-                    placeholder="Enter password"
-                    required />
-            </div>
-            <button type="submit">
-                {loading ? "Loading..." : "Register"}
-            </button>
-        </form>
+    <div className="flex flex-col items-center h-screen justify-center bg-gradient-to-b from-green-600 from-50% to-gray-100 to-50% space-y-6">
+        <h1 className="text-3xl text-white">Registration</h1>
+        <div className="border shadow-lg p-6 w-80 bg-white">
+            <form className="registration-form" onSubmit={handleSubmit}>
+                <div className="mb-4">
+                    <label className="block text-gray-700" htmlFor="email">Email</label>
+                    <input 
+                        className="w-full px-3 py-2 border"
+                        type="text"
+                        id="email"
+                        name="email"
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Enter Email"
+                        required />
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700" htmlFor="password">Password</label>
+                    <input 
+                        className="w-full px-3 py-2 border"
+                        type="password"
+                        id="password"
+                        name="password"
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Enter password"
+                        required />
+                </div>
+                <button 
+                    type="submit"
+                    className="w-full bg-green-600 text-white py-2">
+                    {loading ? "Loading..." : "Register"}
+                </button>
+            </form>
+        </div>
     </div>
   )
 }
